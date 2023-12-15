7ae3a0934717002994d3225fa9d7b209d79c6f8e
Nagababu <ponnapudinagababu1997@gmail.com>
2023-12-15 22:14:59 +0530

    second Commit

--- nagababu/basic_project/src/basic_project/A1Frame.java
+++ nagababu/basic_project/src/basic_project/A1Frame.java
@@ -3,6 +3,7 @@ package basic_project;
 public class A1Frame {
 	public static void main(String[] args) {
 		System.out.println(Runtime.getRuntime().totalMemory());
+		System.out.println(Runtime.getRuntime().totalMemory());
 	}
 	}
 
